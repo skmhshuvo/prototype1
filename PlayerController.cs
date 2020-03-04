ca24402073ae24cbddb93051a9445f1e02c87e49
skmhshuvo <skmhshuvo>
2020-03-05 00:08:03 +0600

    Simple Car Movement Scripts

---
+++ PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float verInput;
+    public float horInput;
+    public float moveSpeed = 30;
+    public float steerAngle = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        verInput = Input.GetAxis("Vertical");
+        horInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * verInput * moveSpeed);
+        transform.Rotate(Vector3.up * horInput * steerAngle);
+    }
+}
